c7921c379bd14379daae80946d87acc908f3f125
Sava <sava.vuckovic@quantox.com>
2019-03-22 14:57:41 +0100

    initial

---
+++ precache-manifest.2d1901728c1dbaf960a4d225dc516729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebdc4a52f4b249963aa6",
+    "url": "/static/js/main.ebdc4a52.chunk.js"
+  },
+  {
+    "revision": "f767ea9d1d11912d989a",
+    "url": "/static/js/1.f767ea9d.chunk.js"
+  },
+  {
+    "revision": "ebdc4a52f4b249963aa6",
+    "url": "/static/css/main.938c4d69.chunk.css"
+  },
+  {
+    "revision": "bcedda90c90822decded6571d50701bb",
+    "url": "/index.html"
+  }
+];
